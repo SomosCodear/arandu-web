7cdfb575ef6c380d83584fbdda6e2c45fe473dd4
Agustin Carrasco <asermax@gmail.com>
2020-10-09 00:17:35 -0300

    refactor(api): make useApi and useCfpBySlug a bit more flexible

--- data/cfp/useCfpBySlug.js
+++ data/cfp/useCfpBySlug.js
@@ -1,3 +1,12 @@
+import { computed } from '@vue/composition-api';
 import { useApi } from '../useApi';
 
-export const useCfpBySlug = (slugRef) => useApi(() => ['cfps', slugRef.value]);
+export const useCfpBySlug = (slugRef, options) => {
+  const keyFunc = () => ['cfps', slugRef.value].join('/');
+  const key = computed(keyFunc);
+
+  return {
+    ...useApi(keyFunc, options),
+    key,
+  };
+};
--- data/useApi.js
+++ data/useApi.js
@@ -1,11 +1,12 @@
 import { useContext } from '@nuxtjs/composition-api';
 import useSWRV from 'swrv';
 
-export const useApi = (key) => {
+export const useApi = (key, options) => {
   const { app } = useContext();
 
   return useSWRV(
     key,
-    ([type, slug]) => app.$axios.$get(`${app.$config.apiUrl}/${type}/${slug}`),
+    (resource) => app.$axios.$get(`${app.$config.apiUrl}/${resource}`),
+    options,
   );
 };
